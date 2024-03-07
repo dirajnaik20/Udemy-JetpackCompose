d96fca225439022c16959c02928dd0dacf84a5ff
Diraj Naik <dirajnaik2020@gmail.com>
2024-03-07 16:20:03 +0530

    stateHoisting/unidirectionDataFlow

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.jetpackcompose
 
-import android.graphics.Paint.Style
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -14,27 +13,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyButton()
+            var count by remember {
+                mutableIntStateOf(0);
+
+            }
+            MyButton(count) { count += 1 }
 
         }
     }
@@ -42,35 +43,31 @@ class MainActivity : ComponentActivity() {
 
 //val count = mutableIntStateOf(0);
 
-@Preview
+
 @Composable
 
-fun MyButton(){
+fun MyButton(counter: Int, updateCount: (Int) -> Unit) {
 
-    val context = LocalContext.current
-    val count = remember {
-        mutableIntStateOf(0);
-
-    }
-
-    Column(modifier = Modifier.fillMaxSize(),
+    Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Button(onClick = {
-            count.value= count.value+1;
-            Toast.makeText(context,
-                "Count: ${count.value}",
-                Toast.LENGTH_SHORT).show()
-        },
-            shape= RectangleShape,
+        Button(
+            onClick = {
+
+                updateCount(counter)
+
+            },
+            shape = RectangleShape,
             contentPadding = PaddingValues(16.dp),
             border = BorderStroke(10.dp, Color.Black),
             colors = ButtonDefaults.buttonColors(Color.Gray)
         ) {
 
-            Text("Count: ${count.value}",
+            Text(
+                "Count: $counter",
                 modifier = Modifier
                     .padding(5.dp),
                 style = MaterialTheme.typography.displaySmall,
