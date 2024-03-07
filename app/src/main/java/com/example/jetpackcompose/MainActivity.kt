6861b0ed6a825564cb941cc1f15a60ca942de2a6
Diraj Naik <dirajnaik2020@gmail.com>
2024-03-07 16:42:54 +0530

    viewModel in Compose

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.jetpackcompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -15,27 +14,24 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var count by remember {
-                mutableIntStateOf(0);
+            val viewModel = viewModel<MyViewModel>()
 
-            }
-            MyButton(count) { count += 1 }
+            val count = viewModel.count
+
+            MyButton(count) { viewModel.updateCount() }
 
         }
     }
---
+++ app/src/main/java/com/example/jetpackcompose/MyViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.jetpackcompose
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class MyViewModel:ViewModel() {
+
+    var count by mutableIntStateOf(0);
+
+    fun updateCount(){
+        count++;
+    }
+
+
+}
